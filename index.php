f4bc7f752d48408157e7072f02f3cc244b06d5e8
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-02-25 02:02:09 -0500

    V1 of Login Page

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<html lang="en">
-
+<html lang="en" style="background-color: #000033;">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,56 +8,31 @@
     <!-- JavaScript -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
 </head>
-
-<body>
-    <!-- <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <img src="images/MCCS-logo-white.png" alt="MCCS logo" width="300" style="align-self: center; padding: 20px">
-                    <h1 class="card-header text-center"> Dashboard Login </h1>
-                    <div class="card-body">
-                        <form action="login_process.php" method="post">
-                        <div class="form-group mt-3">
-                                <label for="username">Username:</label>
-                                <input type="text" name="username" id="username" class="form-control">
-                            </div>
-                            <div class="form-group mt-3">
-                                <label for="password">Password:</label>
-                                <input type="password" name="password" id="password" class="form-control">
-                            </div>
-                            <div class="d-grid gap-2 col-6 mx-auto mt-5">
-                                <button class="btn btn-primary" type="button">Button</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
+<body style="background-color: #000033;">
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <div class="card">
+                <div class="card shadow" style="background-color: #1D1D45;">
                     <img src="images/MCCS-logo-white.png" alt="MCCS logo" width="400" style="align-self: center;" class="mt-3 p-5">
                     <div class="card-header">
                         <h1 class="text-center">Dashboard Login</h3>
                     </div>
                     <div class="card-body">
                         <form>
-                            <div class="mb-4 mt-4">
+                            <div class="mb-4 mt-4 shadow-sm">
                                 <label for="username" class="form-label">Username</label>
                                 <input type="text" id="username" class="form-control" placeholder="Enter your username" required>
                             </div>
-                            <div class="mb-5 mt-4">
+                            <div class="mb-5 mt-4 shadow-sm">
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" id="password" class="form-control" placeholder="Enter your password" required>
                             </div>
                             <div class="d-grid gap-2 col-6 mx-auto">
-                                <button class="btn btn-primary" type="button">Button</button>
+                                <button class="btn-lg shadow" type="button" style="background-color: #CC0000; border: none">Log In <i class="bi bi-arrow-right-circle h4"></i></button>
                             </div>
                         </form>
                     </div>
@@ -67,5 +41,4 @@
         </div>
     </div>
 </body>
-
 </html>
