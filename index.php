6d4eb0d0d2a13a593c8d4eb25d3f11f85fedee0d
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-01 18:36:47 -0500

    Moved in-line styles into CSS style sheets

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<html lang="en" style="background-color: #000033;">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MCCS Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="styles/index.css">
     <!-- JavaScript -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -12,12 +13,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
 </head>
-<body style="color: white; background-color: transparent">
+<body>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <div class="card shadow" style="background-color: #1D1D45;">
-                    <img src="images/MCCS-logo-white.png" alt="MCCS logo" width="250" style="align-self: center;" class="mt-3 p-5">
+                <div class="card shadow">
+                    <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="250" class="mt-3 p-5 MCCS-logo">
                     <div class="card-header">
                         <h1 class="text-center">Dashboard Login</h3>
                     </div>
@@ -32,7 +33,7 @@
                                 <input type="password" id="password" class="form-control form-control-lg" placeholder="Enter your password" required>
                             </div>
                             <div class="d-grid gap-2 col-6 mx-auto">
-                                <button class="btn-lg shadow" type="button" style="background-color: #CC0000; border: none; color: white;">Log In <i class="bi bi-arrow-right-circle h4"></i></button>
+                                <button class="btn-lg shadow login-button" type="button">Log In <i class="bi bi-arrow-right-circle h4"></i></button>
                             </div>
                             <hr class="hr mt-4" />
                             <p class="text-center">Click here to <a href="register.php">register</a></p>
--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<html lang="en" style="background-color: #000033;">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -6,20 +6,21 @@
     <title>MCCS Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="styles/index.css">
     <!-- JavaScript -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
+    
 </head>
 
-<body style="background-color: #000033; color: white;">
+<body>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
-                <div class="card shadow" style="background-color: #1D1D45;">
-                    <img src="images/MCCS-logo-white.png" alt="MCCS logo" width="250" style="align-self: center;" class="mt-1 p-5">
+                <div class="card shadow">
+                <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="250" class="mt-3 p-5 MCCS-logo">
                     <div class="card-header">
                         <h2 class="text-center">Dashboard Registration</h3>
                     </div>
@@ -29,7 +30,7 @@
                                 <div class="mb-4 mt-4 shadow-sm col">
                                     <label for="fold" class="form-label">Role</label>
                                     <div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle form-control" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false" style="background-color: #CC0000; border: none; color: white;">
+                                        <button class="btn btn-secondary dropdown-toggle form-control role-button" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
                                             Role
                                         </button>
                                         <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
@@ -56,7 +57,7 @@
                                 <input type="password" id="password" class="form-control form-control" placeholder="Enter your password" required>
                             </div>
                             <div class="d-grid gap-2 col-6 mx-auto">
-                                <button class="btn-lg shadow" type="button" style="background-color: #CC0000; border: none; color: white;">Register <i class="bi bi-arrow-right-circle h4"></i></button>
+                                <button class="btn-lg shadow login-button" type="button">Register <i class="bi bi-arrow-right-circle h4"></i></button>
                             </div>
                             <hr class="hr mt-4" />
                             <p class="text-center">Click here to <a href="index.php">login</a></p>
