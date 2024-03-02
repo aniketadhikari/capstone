30c2fd27785ad7f4e6a4be20d58f9882e15f53e1
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-01 22:32:59 -0500

    Worked a little on the welcome screen

--- welcome-screen.php
+++ welcome-screen.php
@@ -17,31 +17,23 @@ session_start();
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Professor Dashboard</title>
+   <title>Dashboard</title>
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="../css/styles.css">
-   <link rel="shortcut icon" href="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1408402010/bxqs0rvkbgqwgnnfnhu0.jpg">
-   <style>
-      .professor-body {
-         background: url('../images/blurred_Singapore_Management_University_School_of_Law_Armeninan_Street.jpg');
-         background-size: cover;
-         background-repeat: repeat-y;
-      }
-   </style>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
+
 
 <body class="professor-body">
    <div class="container">
-      <div class="content">
-         <h3>Hello, <span>professor</span></h3>
-         <h1>Welcome <span><?php echo $_SESSION['professor_name'] ?></span></h1>
-         <a href="operational_reporting.php" class="btn">Operational Reporting</a>
-         <a href="schedule_eval.php" class="btn">Evaluations</a>
-         <a href="groups.php" class="btn">Groups</a>
-         <a href="import_students.php" class="btn">Import Students</a>
-         <a href="import_courses.php" class="btn">Import Courses</a>
-         <a href="../logout.php" class="btn">Logout</a>
+      <div class="content text-center">
+         <a href="dashboard.php" class="btn">Dashboard</a>
+         <a href="account_settings.php" class="btn">Account Settings</a>
+         <a href="vendor_ratings.php" class="btn">Vendor Rating</a>
+         <a class="navbar-brand" href="#">
+      <img src="images/login-button.png" alt="Logo" width="30" height="24">Stores</a>
+         <a href="logout.php" class="btn">Logout</a>
       </div>
    </div>
 </body>
