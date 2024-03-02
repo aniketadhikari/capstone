ab6ed5868c650eb84162804c6cc3316c099b0f65
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-02 14:03:51 -0500

    Added styles

--- welcome-screen.php
+++ welcome-screen.php
@@ -17,7 +17,7 @@ session_start();
    <meta charset="UTF-8">
    <title>Welcome Screen</title>
 
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
    <link rel="stylesheet" href="styles/welcome-screen.css">
    <!-- custom css file link  -->
@@ -25,12 +25,12 @@ session_start();
 </head>
 
 
-<body class="professor-body">
+<body>
    <div class="container mt-5">
       <div class="row justify-content-center">
-         <div class="card shadow">
+         <div class="card shadow navigation-card">
             <div class="card-header">
-               <h1 class="text-center">Dashboard Login</h1>
+               <h1 class="text-center" style="color: white;">Welcome</h1>
             </div>
             <div class="card-body">
                <div class="content text-center">
@@ -38,7 +38,7 @@ session_start();
                   <a href="account_settings.php" class="btn">Account Settings</a>
                   <a href="vendor_ratings.php" class="btn">Vendor Rating</a>
                   <a href="stores.php" class="btn">Stores</a>
-                  <a href="logout.php" class="btn">Logout</a>
+                  <a href="index.php" class="btn">Logout</a>
                </div>
             </div>
          </div>
