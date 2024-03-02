c355c78540250c5c23e75c9ecbe08e30cf7ba01d
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-02 00:09:38 -0500

    Worked on Welcome Screen

--- welcome-screen.php
+++ welcome-screen.php
@@ -8,33 +8,42 @@ session_start();
 //    header('location:../index.php');
 // }
 
-?> 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Dashboard</title>
+   <title>Welcome Screen</title>
 
-   <!-- custom css file link  -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <link rel="stylesheet" href="styles/welcome-screen.css">
+   <!-- custom css file link  -->
+
 </head>
 
 
-
 <body class="professor-body">
-   <div class="container">
-      <div class="content text-center">
-         <a href="dashboard.php" class="btn">Dashboard</a>
-         <a href="account_settings.php" class="btn">Account Settings</a>
-         <a href="vendor_ratings.php" class="btn">Vendor Rating</a>
-         <a class="navbar-brand" href="#">
-      <img src="images/login-button.png" alt="Logo" width="30" height="24">Stores</a>
-         <a href="logout.php" class="btn">Logout</a>
+   <div class="container mt-5">
+      <div class="row justify-content-center">
+         <div class="card shadow">
+            <div class="card-header">
+               <h1 class="text-center">Dashboard Login</h1>
+            </div>
+            <div class="card-body">
+               <div class="content text-center">
+                  <a href="dashboard.php" class="btn">Dashboard</a>
+                  <a href="account_settings.php" class="btn">Account Settings</a>
+                  <a href="vendor_ratings.php" class="btn">Vendor Rating</a>
+                  <a href="stores.php" class="btn">Stores</a>
+                  <a href="logout.php" class="btn">Logout</a>
+               </div>
+            </div>
+         </div>
       </div>
    </div>
 </body>
+
 </html>
