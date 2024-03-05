3d315539d22baa74253e2918f37f986936f14e22
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-05 12:23:15 -0500

    Changes to nav bar and

--- dashboard.php
+++ dashboard.php
@@ -7,15 +7,28 @@
     <title>Dashboard</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/navigation.css?1">
+    <link rel="stylesheet" href="styles/dashboard.css">
 </head>
+
+<header class="text-center display-3">
+    <p>Dashboard</p>
+</header>
 
 <body>
     <?php
     @include 'navigation.php';
     ?>
-    <div class="container" >
-        <h1>Dashboard</h1>
-        <!-- Your dashboard content goes here -->
+    <div>
+        <div class="container">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Card Title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
--- navigation.php
+++ navigation.php
@@ -6,18 +6,19 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Login</title>
+   <style>
+      img {}
+   </style>
    <!-- custom css file link  -->
    <link rel="shortcut icon" href="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1408402010/bxqs0rvkbgqwgnnfnhu0.jpg">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-   <link rel="stylesheet" href="styles/navigation.css">
-   <style>
-   </style>
-   <div class="sidebar container">
-      <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="250" class="mt-3 p-5 MCCS-logo">
+   <link rel="stylesheet" href="styles/navigation.css?1">
+   <div class="position-absolute top-0 sidebar">
+         <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="300px" class="mt-3 p-5 MCCS-logo">
       <a href="welcome.php" class="nav-btn">Home</a>
       <a href="dashboard.php" class="active nav-btn">Dashboard</a>
       <a href="stores.php" class="nav-btn">Stores</a>
       <a href="ratings.php" class="nav-btn">Vendor Ratings</a>
    </div>
-
+u
 </html>
