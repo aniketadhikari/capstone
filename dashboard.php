fb3fb0ec5bb15a8cc93250f8882e38ff0a5928f7
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-05 12:40:07 -0500

    Sticky sidebar and some example cards

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="styles/navigation.css?1">
-    <link rel="stylesheet" href="styles/dashboard.css">
+    <link rel="stylesheet" href="styles/pages.css">
 </head>
 
 <header class="text-center display-3">
@@ -16,16 +16,47 @@
 </header>
 
 <body>
-    <?php
-    @include 'navigation.php';
-    ?>
+        <?php
+        @include 'navigation.php';
+        ?>
     <div>
-        <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="container mt-3">
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- navigation.php
+++ navigation.php
@@ -6,19 +6,17 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Login</title>
-   <style>
-      img {}
-   </style>
+</head>
    <!-- custom css file link  -->
    <link rel="shortcut icon" href="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1408402010/bxqs0rvkbgqwgnnfnhu0.jpg">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
    <link rel="stylesheet" href="styles/navigation.css?1">
-   <div class="position-absolute top-0 sidebar">
-         <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="300px" class="mt-3 p-5 MCCS-logo">
+   <div class="position-fixed top-0 sidebar">
+      <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="325px" class="mt-3 p-5 MCCS-logo">
       <a href="welcome.php" class="nav-btn">Home</a>
       <a href="dashboard.php" class="active nav-btn">Dashboard</a>
       <a href="stores.php" class="nav-btn">Stores</a>
       <a href="ratings.php" class="nav-btn">Vendor Ratings</a>
    </div>
-u
+
 </html>
