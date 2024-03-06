d5674ad97830ee314222e046d1278282c7f4c3de
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-06 14:21:53 -0500

    Worked on nav bar again and cards on dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,54 +7,75 @@
     <title>Dashboard</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/pages.css?1">
     <link rel="stylesheet" href="styles/navigation.css?1">
-    <link rel="stylesheet" href="styles/pages.css">
 </head>
 
-<header class="text-center display-3">
+<header class="text-center display-3 mx-auto">
     <p>Dashboard</p>
 </header>
 
 <body>
-        <?php
-        @include 'navigation.php';
-        ?>
+    <?php
+    @include 'navigation.php';
+    ?>
     <div>
-        <div class="container mt-3">
-            <div class="row row-cols-1 row-cols-md-2 g-4">
-                <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+        <div id="screen" class="container mt-2 mb-2">
+            <div class="row row-cols-1 mb-5 mt-5">
+                <div class="col-9">
+                    <div id="widget" class="card text-white h-100">
+                        <div class="card-header">Header</div>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Dark card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div id="widget" class="card text-white h-100">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Dark card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row row-cols-1 row-cols-md-4 mt-4">
+                <div class="col">
+                    <div id="widget" class="card text-white mb-3">
+                        <div class="card-header">Header</div>
+                        <div class="card-body">
+                            <h5 class="card-title">Dark card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                    <div id="widget" class="card text-white mb-3">
+                        <div class="card-header">Header</div>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Dark card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                    <div id="widget" class="card text-white mb-3">
+                        <div class="card-header">Header</div>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            <h5 class="card-title">Dark card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                    <div id="widget" class="card text-white mb-3">
+                        <div class="card-header">Header</div>
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Dark card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
                     </div>
                 </div>
--- navigation.php
+++ navigation.php
@@ -6,17 +6,35 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Login</title>
-</head>
-   <!-- custom css file link  -->
    <link rel="shortcut icon" href="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/v1408402010/bxqs0rvkbgqwgnnfnhu0.jpg">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-   <link rel="stylesheet" href="styles/navigation.css?1">
-   <div class="position-fixed top-0 sidebar">
-      <img src="images/MCCS-logo-white.png" id="MCCS-logo" alt="MCCS logo" width="325px" class="mt-3 p-5 MCCS-logo">
-      <a href="welcome.php" class="nav-btn">Home</a>
-      <a href="dashboard.php" class="active nav-btn">Dashboard</a>
-      <a href="stores.php" class="nav-btn">Stores</a>
-      <a href="ratings.php" class="nav-btn">Vendor Ratings</a>
+   <link rel="stylesheet" href="styles/navigation.css">
+</head>
+<!-- custom css file link  -->
+
+<div class="position-fixed top-0 sidebar">
+   <img src="images/MCCS-logo-white.png" alt="MCCS logo" class="MCCS-logo mx-auto mt-4 d-block">
+   <div>
+      <a href="welcome.php" class="nav-btn fs-3">Home</a>
+      <a href="dashboard.php" class="active nav-btn fs-3">Dashboard</a>
+      <a href="stores.php" class="active nav-btn fs-3">Stores</a>
+      <a href="ratings.php" class="active nav-btn fs-3">Vendor Ratings</a>
    </div>
+   <div class="options row position-fixed bottom-0">
+      <div class="col-1 option"></div>
+      <div class="col-3 option">
+         <a href="settings.php">
+            <img class="image-options mx-auto d-block" src="images/sf-icons/profile.png" width="24px" alt="">
+         </a>
+      </div>
+      <div class="col-4 option"></div>
+      <div class="col-3 option">
+         <a href="logout.php">
+            <img class="image-options mx-auto d-block" src="images/sf-icons/logout.png" width="25px" alt="">
+         </a>
+      </div>
+   </div>
+   <div class="col-1 option"></div>
+</div>
 
 </html>
