7b5090780f11540cb69e08ab6060dd5004cdfecf
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-09 22:38:03 -0500

    (Finally) Fixed

--- register.php
+++ register.php
@@ -4,40 +4,23 @@
 
 if (isset($_POST['submit'])) {
 
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = md5($_POST['password']);
-    $user_type = $_POST['user_type'];
 
-    $validate_count = 0;
-    // Check if the user is allowed to register with the student email
-    $validate_students = "SELECT * FROM Student WHERE EmailAddress = '$email'";
-    $validate_student_q = mysqli_query($conn, $validate_students);
-    $validate_count += mysqli_num_rows($validate_student_q);
-    // check if the user is allowed to register with the professor email 
-    $validate_professors = "SELECT * FROM Professor WHERE EmailAddress = '$email'";
-    $validate_professor_q = mysqli_query($conn, $validate_professors);
-    $validate_count += mysqli_num_rows($validate_professor_q);
-    // Check if the user has already been registered
-    $duplicates = " SELECT * FROM Users WHERE EmailAddress = '$email'";
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
+    $role = isset($_POST['user_type']) ? $_POST['user_type'] : '';
+
+    $duplicates = "SELECT * FROM dbmaster.Users WHERE email = '$email'";
     $duplicates_q = mysqli_query($conn, $duplicates);
-    if ($validate_count == 0) {
-        $error[] = 'user not cleared to register!';
-    } elseif (mysqli_num_rows($duplicates_q) > 0) {
+
+    if (mysqli_num_rows($duplicates_q) > 0) {
         $error[] = 'user already exist!';
     } else {
-        if ($pass != $cpass) {
-            $error[] = 'password not matched!';
-        } else {
-            $insert = "INSERT INTO Users(FirstName, LastName, EmailAddress, Password, UserType) VALUES('$first_name','$last_name','$email','$pass','$user_type')";
-            mysqli_query($conn, $insert);
-            header('location:index.php');
-        }
+        $insert = "INSERT INTO dbmaster.Users(username, password, role, email) VALUES('$username','$password','$role','$email')";
+        mysqli_query($conn, $insert);
+        header('location:index.php');
     }
 }
-
-
-
 ?>
 
 <html lang="en">
@@ -67,7 +50,7 @@ if (isset($_POST['submit'])) {
                         <h1 class="text-center">Registration</h1>
                     </div>
                     <div class="card-body">
-                        <form>
+                        <form action="" method="post">
                             <div class="row g-3">
                                 <div class="mb-4 mt-4 shadow-sm col">
                                     <label for="fold" class="form-label">Role</label>
@@ -84,19 +67,26 @@ if (isset($_POST['submit'])) {
                                 </div>
                                 <div class="mb-4 mt-4 shadow-sm col">
                                     <label for="email" class="form-label">Email</label>
-                                    <input type="email" id="email" class="form-control" placeholder="Enter your email address" required>
+                                    <input name="email" type="email" id="email" class="form-control" placeholder="Enter your email address" required>
                                 </div>
                             </div>
                             <div class="mb-4 shadow-sm">
                                 <label for="username" class="form-label">Username</label>
-                                <input type="text" id="username" class="form-control" placeholder="Enter your username" required>
+                                <input name="username" type="text" id="username" class="form-control" placeholder="Enter your username" required>
                             </div>
                             <div class="mb-4 mt-4 shadow-sm">
                                 <label for="password" class="form-label">Password</label>
-                                <input type="password" id="password" class="form-control form-control" placeholder="Enter your password" required>
+                                <input name="password" type="password" id="password" class="form-control form-control" placeholder="Enter your password" required>
                             </div>
                             <div class="d-grid gap-2 col-6 mx-auto">
                                 <button class="btn-lg shadow login-button" type="submit" name="submit">Register <i class="bi bi-arrow-right-circle h4"></i></button>
+                                <?php
+                                if (isset($error)) {
+                                    foreach ($error as $error) {
+                                        echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
+                                    };
+                                }
+                                ?>
                             </div>
                             <hr class="hr mt-4" />
                             <p class="text-center">Click here to <a href="index.php">login</a></p>
